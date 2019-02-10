4b5b1e4f12b753d19bdaa3b36cc8a6bac1fbf9e6
Jan Gareev <g.zhan.m@gmail.com>
2019-02-10 20:27:07 +0300

    site

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import '../node_modules/jquery/dist/jquery.js'
+import '../node_modules/what-input/dist/what-input'
+import '../node_modules/foundation-sites/dist/js/foundation'
+//import './bar'
+//import '../node_modules/bootstrap/dist/js/bootstrap'
+
+//import vue from 'vue'
+//window.Vue = require('vue')
